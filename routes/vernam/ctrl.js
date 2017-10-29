4a95c74c344f169618c3488fcca55107215dce88
Jermaine Coates <jermainecoates@outlook.com>
2017-10-29 13:59:40 -0500

    Update Vernam Cipher

--- routes/vernam/ctrl.js
+++ routes/vernam/ctrl.js
@@ -1,12 +1,68 @@
 'use strict'
 
+let alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 
+				'h', 'i', 'j', 'k', 'l', 'm', 'n', 
+				'o', 'p', 'q', 'r', 's', 't', 'u', 
+				'v', 'w', 'x', 'y', 'z']
 let vernam = {
-  encrypt: (req, res)=>{
-    return res.json("Encrypt")
-  },
-  decrypt: (req, res)=>{
-    return res.json("Encrypt")
-  },
+	encrypt: (req, res)=>{
+		let encryptedMsg = ""
+	  	let values = {	
+	      key: req.body.key,
+	      msg: req.body.msg
+	    }
+	    if (values.key.length != values.msg.length)
+    		return res.json("Invaild Key") 
+
+	    for (let i = 0; i <  values.msg.length; i++) {
+	    	if (values.msg.charAt(i) == " "){
+	    		encryptedMsg += " " 
+	    	}else {
+	    		for (let j = 0; j < alphabet.length; j++) {
+	    			if(values.msg.charAt(i).toLowerCase() == alphabet[j]) {
+	    				for (let k = 0; k < alphabet.length; k++) {
+	    					if(values.key.charAt(i).toLowerCase() == alphabet[k]) {
+		    					let index = ((j + k) % 26)
+		    					encryptedMsg += alphabet[index]
+		    					break;	
+	    					}
+	    				}    			
+	    			}
+	    		}
+	    	}
+	    }
+
+	    return res.json(encryptedMsg)
+  	},
+  	decrypt: (req, res)=>{
+  		let decryptedMsg = ""
+	  	let values = {	
+	      key: req.body.key,
+	      msg: req.body.msg
+	    }
+	    if (values.key.length != values.msg.length)
+    		return res.json("Invaild Key") 
+
+	    for (let i = 0; i <  values.msg.length; i++) {
+	    	if (values.msg.charAt(i) == " "){
+	    		decryptedMsg += " " 
+	    	}else {
+	    		for (let j = 0; j < alphabet.length; j++) {
+	    			if(values.msg.charAt(i).toLowerCase() == alphabet[j]) {
+	    				for (let k = 0; k < alphabet.length; k++) {
+	    					if(values.key.charAt(i).toLowerCase() == alphabet[k]) {
+		    					let index = ((j - k) % 26)
+		    					decryptedMsg += alphabet[index]
+		    					break;	
+	    					}
+	    				}    			
+	    			}
+	    		}
+	    	}
+	    }
+
+	    return res.json(decryptedMsg)
+	},
 }
 
 exports.vernam = vernam
